55362c387829350db3ab2565cf3d502bc681c98f
Jose M. Cantera <jose.cantera@iota.org>
2025-04-30 08:35:26 +0200

    fix: linting issues

--- packages/standards-dublin-core/src/models/dublinCoreClasses.ts
+++ packages/standards-dublin-core/src/models/dublinCoreClasses.ts
@@ -7,16 +7,14 @@
  */
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const DublinCoreClasses = {
-    /**
-     * See Description at
-     * https://www.dublincore.org/specifications/dublin-core/dcmi-terms/#http://purl.org/dc/dcmitype/Collection
-     */
-    Collection: "Collection"
+	/**
+	 * See Description at
+	 * https://www.dublincore.org/specifications/dublin-core/dcmi-terms/#http://purl.org/dc/dcmitype/Collection
+	 */
+	Collection: "Collection"
 } as const;
-
 
 /**
  * Dublin Core classes
  */
-export type DublinCoreClasses =
-	(typeof DublinCoreClasses)[keyof typeof DublinCoreClasses];
+export type DublinCoreClasses = (typeof DublinCoreClasses)[keyof typeof DublinCoreClasses];
